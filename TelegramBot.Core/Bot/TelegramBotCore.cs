40c373cdb24c24fa238a9982632e7d35a4d674de
Vladyslav Panasenko <vladyslav_panasenko@epam.com>
2018-08-02 12:15:24 +0300

    Added factory

--- TelegramBot.Core/Bot/TelegramBotCore.cs
+++ TelegramBot.Core/Bot/TelegramBotCore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
 using TelegramBot.Core.Interfaces;
 using TelegramBot.Infrastucture.Contracts;
 using TelegramBot.Infrastucture.Interfaces;
@@ -12,11 +13,13 @@ namespace TelegramBot.Core.Bot
     {
         private readonly IWeatherPhraseDecorator _weatherPhraseDecorator;
         private readonly Settings _settings;
+        private readonly ITelegramBotClient _client;
 
-        public TelegramBotCore(IWeatherPhraseDecorator weatherPhraseDecorator, Settings settings)
+        public TelegramBotCore(IWeatherPhraseDecorator weatherPhraseDecorator, Settings settings, ITelegramBotClient client)
         {
             _weatherPhraseDecorator = weatherPhraseDecorator;
             _settings = settings;
+            _client = client;
         }
 
         public void Run()
---
+++ TelegramBot.Infrastucture/Services/TelegramBotClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot;
+
+namespace TelegramBot.Infrastucture.Services
+{
+    public static class TelegramBotClientFactory
+    {
+        public static ITelegramBotClient GetClient(string token)
+        {
+           return new TelegramBotClient(token);
+        }
+    }
+}
