56a26f0e27a3b7f53eb36f9427a08c2599bd2328
Vladyslav Panasenko <vladyslav_panasenko@epam.com>
2018-08-02 11:37:46 +0300

    Added constructor to telegram bot core.

--- TelegramBot.Core/Bot/TelegramBotCore.cs
+++ TelegramBot.Core/Bot/TelegramBotCore.cs
@@ -4,14 +4,19 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 using TelegramBot.Core.Interfaces;
 using TelegramBot.Infrastucture.Contracts;
+using TelegramBot.Infrastucture.Interfaces;
 
 namespace TelegramBot.Core.Bot
 {
     public class TelegramBotCore : ITelegramBotCore
     {
-        public TelegramBotCore(Settings settings)
+        private readonly IWeatherPhraseDecorator _weatherPhraseDecorator;
+        private readonly Settings _settings;
+
+        public TelegramBotCore(IWeatherPhraseDecorator weatherPhraseDecorator, Settings settings)
         {
-            var s = settings;
+            _weatherPhraseDecorator = weatherPhraseDecorator;
+            _settings = settings;
         }
 
         public void Run()
