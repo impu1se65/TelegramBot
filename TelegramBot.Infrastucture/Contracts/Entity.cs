2a2bf310eb25d86fba391307e82bf4d1e7a46676
Vladyslav Panasenko <vladyslav_panasenko@epam.com>
2018-08-13 16:13:36 +0300

    fixed

--- TelegramBot.Infrastucture/Contracts/Entity.cs
+++ TelegramBot.Infrastucture/Contracts/Entity.cs
@@ -3,10 +3,15 @@
     public class Entity
     {
         public string EntityType { get; set; }
+
         public string Type { get; set; }
+
         public int StartIndex { get; set; }
+
         public int EndIndex { get; set; }
+
         public double Score { get; set; }
+
         public Resolution Resolution { get; set; }
     }
 }
